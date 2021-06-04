f592ad60b4decc1055341b049c685c05d945e6db
RC-Midori <80474854+RC-Midori@users.noreply.github.com>
2021-06-04 14:34:59 -0600

    Update conexion.php

--- database/conexion.php
+++ database/conexion.php
@@ -1,8 +1,8 @@
 <?php
 	$host = "localhost";
-	$database='partnumber';
-	$user='root';
-	$password='';
+	$database='id16620177_partnumber';
+	$user='id16620177_root';
+	$password='a}@z=6O?AA_0N$wI';
 
 	$con=mysqli_connect($host,$user,$password);
 	mysqli_select_db($con, $database);
